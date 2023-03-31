3f007d5c5bca489884f2a79867a966b8e93371f6
Juanma <119064242+JuanmaBenitez73@users.noreply.github.com>
2023-03-31 11:35:01 +0200

    aa

---
+++ Juanma/javascript.js
@@ -0,0 +1,61 @@
+const pokemonList = document.querySelector("#pokemon-list");
+var URL = "https://pokeapi.co/api/v2/pokemon/";
+
+for(var i = 1; i <= 151; i++)
+{
+    fetch(URL + i)
+        .then((response) => response.json())
+        .then(datos => verPokemon(datos))
+}
+
+function verPokemon(poke)
+{
+    var tipos = poke.types.map(type => `<p class="type ${type.type.name}">${type.type.name}</p>`);
+    tipos = tipos.join('');
+    
+    var pokeId = poke.id.toString();
+    if(pokeId.length === 1)
+    {
+        pokeId = "00" + pokeId;
+    }
+    else if(pokeId.length === 2)
+    {
+        pokeId = "0" + pokeId;
+    }
+
+    const div = document.createElement("div");
+    div.classList.add("pokemon");
+    div.innerHTML = `
+            <div class="image">
+                <img src="${poke.sprites.other.home.front_default}" alt="${poke.name}">
+            </div>
+            <div class="info">
+                <div class="container">
+                    <p class="id">#${poke.id}</p>
+                    <h2 class="name">${poke.name}</h2>
+                </div>
+                <div class="types">
+                    ${tipos}
+                </div>
+            </div>
+            `;
+    pokemonList.append(div);
+}
+
+/*
+    <div class="pokemon">
+        <div class="image">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png" alt="Pikachu">
+        </div>
+        <div class="info">
+            <div class="container">
+                <p class="id">#025</p>
+                <h2 class="name">Pikachu</h2>
+            </div>
+            <div class="types">
+                <p class="type electric">Eléctrico</p>
+                <p class="type dragon">Eléctrico</p>
+            </div>
+        </div>
+    </div>
+*/
