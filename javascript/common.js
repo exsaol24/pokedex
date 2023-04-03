f569a2692b47369fd9eeab48aea9dcfc06abc884
Juanma <119064242+JuanmaBenitez73@users.noreply.github.com>
2023-04-03 19:14:10 +0200

    aa

---
+++ javascript/common.js
@@ -0,0 +1,37 @@
+var tipoPokemmon = {
+    normal: 'Normal',
+    fire: 'Fuego',
+    water: 'Water',
+    grass: 'Planta',
+    electric: 'Eléctrico',
+    ice: 'Hielo',
+    fighting: 'Lucha',
+    poison: 'Veneno',
+    ground: 'Tierra',
+    flying: 'Volador',
+    psychic: 'Psíquico',
+    bug: 'Bicho',
+    rock: 'Roca',
+    ghost: 'Fantasma',
+    dark: 'Siniestro',
+    dragon: 'Dragón',
+    steel: 'Acero',
+    fairy: 'Hada',
+};
+
+/*function translateToSpanish(textToTranslate) {
+    const translatedText = [];
+
+    const words = textToTranslate.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        if (tipoPokemmon[word]) {
+            translatedText.push(tipoPokemmon[word]);
+        } else {
+            translatedText.push(word);
+        }
+    }
+
+    return translatedText.join(' ');
+}*/
