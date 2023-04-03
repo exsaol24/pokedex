4cd645ae0aba7bf3738aa0c57b6ec7a4e09e6390
Juanma <119064242+JuanmaBenitez73@users.noreply.github.com>
2023-04-03 18:06:23 +0200

    aa

--- javascript/lista.js
+++ javascript/lista.js
@@ -1,44 +1,47 @@
 const pokemonList = document.querySelector("#pokemon-list");
 const URL = "https://pokeapi.co/api/v2/pokemon/";
 
-for (var i = 1; i <= 151; i++) {
-    fetch(URL + i)
-        .then(respuesta => respuesta.json())
-        .then(respuesta => verPokemon(respuesta))
+async function getPokemon() {
+    for (var i = 1; i <= 151; i++) {
+        var request = await fetch(URL + i);
+        var data = await request.json();
+        await viewPokemon(data);
+    }
 }
 
-/*async*/ function verPokemon(poke) {
+function viewPokemon(poke) {
     var tipos = poke.types.map(type => `<p class="type ${type.type.name}">${type.type.name}</p>`);
     tipos = tipos.join('');
-    
-    const div1 = document.createElement("div");
+
+    var div1 = document.createElement("div");
     div1.classList.add("pokemon");
 
     /*
-    const a = document.createElement("a");
+    var a = document.createElement("a");
     a.classList.add("enlace");
 
-    const div2 = document.createElement("div");
+    var div2 = document.createElement("div");
     div2.classList.add("image");
 
-    const img = document.createElement("img");
-    img.src(poke.sprites.front_default);
+    var img = document.createElement("img");
+    img.setAttribute("src", "poke.sprites.front_default");
+    img.setAttribute("alt", "poke.name");
     
-    const div3 = document.createElement("div");
+    var div3 = document.createElement("div");
     div3.classList.add("info");
 
-    const div4 = document.createElement("div");
+    var div4 = document.createElement("div");
     div4.classList.add("container");
 
-    const p = document.createElement("p");
+    var p = document.createElement("p");
     p.classList.add("id");
     p.innerHTML = `#${id0Izqda(poke)}`;
 
-    const h2 = document.createElement("h2");
+    var h2 = document.createElement("h2");
     h2.classList.add("name");
     h2.innerHTML = `${poke.name}`;
 
-    const div5 = document.createElement("div");
+    var div5 = document.createElement("div");
     div5.classList.add("types");
     div5.innerHTML = `${tipos}`;
     */
@@ -73,10 +76,10 @@ for (var i = 1; i <= 151; i++) {
 
 function id0Izqda(poke) {
     var pokeId = poke.id.toString();
-    if (pokeId.length === 1) {
+    if (pokeId.length == 1) {
         pokeId = "00" + pokeId;
     }
-    else if (pokeId.length === 2) {
+    else if (pokeId.length == 2) {
         pokeId = "0" + pokeId;
     }
 
